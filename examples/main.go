523b8604ca23e5baf34798d8de59613f45467a4e
vspaz <vladimir.edelman@gmail.com>
2022-12-19 18:33:35 +0100

    add dummy go example

--- examples/main.go
+++ examples/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"rmqclient/pkg/rmq"
+)
 
+func main() {
+	rmqClient := rmq.New()
+	connection := rmqClient.Connect()
+	defer rmqClient.CloseConnection(connection)
+	channel := rmqClient.CreateChannel(connection)
+	defer rmqClient.CloseChannel(channel)
+	rmqClient.DeclareExchange(channel)
+	message := "foobar"
+	if err := rmqClient.PublishTask(channel, []byte(message)); err != nil {
+		fmt.Errorf("error occured %s", message)
+	}
 }
